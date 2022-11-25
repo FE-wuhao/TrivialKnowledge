c22b22a2ca17936ae32f0ede83f76e98ecc7d86a
吴灏 <wuh@cnlemon.net>
2022-11-25 15:03:50 +0800

    feat: 策略模式完成

--- designPatterns/policy.js
+++ designPatterns/policy.js
@@ -2,35 +2,35 @@
  * @Author: nhsoft.wh
  * @Date: 2022-11-25 14:56:22
  * @LastEditors: nhsoft.wh
- * @LastEditTime: 2022-11-25 15:02:43
+ * @LastEditTime: 2022-11-25 15:03:45
  * @Description:
  * 策略模式：将运算转化为配置，通过映射的方式获取多分支的结果
  */
 function getPriceRangeByType(type) {
-  if (type === "原价") {
+  if (type === "originalPrice") {
     return [1, 2];
-  } else if (type === "出厂价") {
+  } else if (type === "exFactoryPrice") {
     return [2, 3];
-  } else if (type === "划线价") {
+  } else if (type === "crossedPrice") {
     return [4, 5];
-  } else if (type === "限时折扣") {
+  } else if (type === "limitedTimeDiscount") {
     return [3, 4];
   }
 }
 
 console.log(
   "通过if-else的方式获取出厂价的价格区间：",
-  getPriceRangeByType("出厂价")
+  getPriceRangeByType("exFactoryPrice")
 );
 
 const priceRangeMapper = {
-  原价: [1, 2],
-  出厂价: [2, 3],
-  划线价: [4, 5],
-  限时折扣: [3, 4],
+  originalPrice: [1, 2],
+  exFactoryPrice: [2, 3],
+  crossedPrice: [4, 5],
+  limitedTimeDiscount: [3, 4],
 };
 
 console.log(
   "通过策略模式的方式获取出厂价的价格区间：",
-  priceRangeMapper["出厂价"]
+  priceRangeMapper["exFactoryPrice"]
 );
